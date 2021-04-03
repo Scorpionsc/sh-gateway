fd3290a07920f25ddc45de1bea2edf7fff2d0b82
Mykola Petrunin <petruninnikolay@gmail.com>
2021-04-03 15:04:19 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
 import { createExpressServer } from 'routing-controllers';
 import { UserController } from './controller/user-controller';
+import { HomeController } from './controller/home-controller';
 
 dotenv.config();
 
 const port = process.env.PORT;
 
 const app = createExpressServer({
-  controllers: [UserController]
+  controllers: [UserController, HomeController]
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
