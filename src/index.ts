f3b31da69092e73edb1c3d10f52746f3cb2be0d2
Mykola Petrunin <petruninnikolay@gmail.com>
2021-04-04 18:10:35 +0300

    add sc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import { createExpressServer } from 'routing-controllers';
 import { HomeController } from './controller/home-controller.js';
-import {SugarCollectorController} from './controller/sugar-colector-controller.js';
+import { SugarCollectorController } from './controller/sugar-colector-controller.js';
 
 dotenv.config();
 
