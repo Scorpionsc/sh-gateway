9e745a715ea30e2cb14e6f2c838fde5d24df03d9
Mykola Petrunin <petruninnikolay@gmail.com>
2021-04-04 18:06:06 +0300

    add sc

--- src/controller/home-controller.ts
+++ src/controller/home-controller.ts
@@ -1,21 +1,9 @@
 import { Controller, Get } from 'routing-controllers';
-import SugarCollector from '../services/SugarCollectorData.js';
 
 @Controller()
 export class HomeController {
     @Get('/')
   get () {
-    const test = async () => {
-      const sugarCollector = SugarCollector.instance;
-      const lastEntry = await sugarCollector.getLastEntry();
-
-      const loopData = await sugarCollector.getLoopData();
-      return {
-        lastEntry,
-        loopData
-      };
-    };
-
-    return test();
+    return 'API works';
   }
 }
---
+++ src/controller/sugar-colector-controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get } from 'routing-controllers';
+import SugarCollector from '../services/SugarCollectorData.js';
+
+@Controller()
+export class SugarCollectorController {
+    @Get('/sugar-collector')
+  get () {
+    const test = async () => {
+      const sugarCollector = SugarCollector.instance;
+      const lastEntry = await sugarCollector.getLastEntry();
+
+      const loopData = await sugarCollector.getLoopData();
+      return {
+        lastEntry,
+        loopData
+      };
+    };
+
+    return test();
+  }
+}
