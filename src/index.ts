2fc7484dc7e1fa4eb218ebb5f4e13b3489d4434c
Mykola Petrunin <petruninnikolay@gmail.com>
2021-04-04 22:14:41 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,15 @@
-import express from 'express';
 import dotenv from 'dotenv';
-// import { createExpressServer } from 'routing-controllers';
-// import { HomeController } from './controller/home-controller.js';
-// import { SugarCollectorController } from './controller/sugar-colector-controller.js';
-import SugarCollector from './services/SugarCollectorData.js';
+import { createExpressServer } from 'routing-controllers';
+import { HomeController } from './controller/home-controller.js';
+import { SugarCollectorController } from './controller/sugar-colector-controller.js';
 
 dotenv.config();
 
 const port = process.env.PORT;
-const app = express();
-// const app = createExpressServer({
-//   controllers: [SugarCollectorController, HomeController]
-// });
-
-app.get('/sugar-collector', async (req, res) => {
-  const sugarCollector = SugarCollector.instance;
-  const lastEntry = await sugarCollector.getLastEntry();
-
-  const loopData = await sugarCollector.getLoopData();
-  res.send({
-    lastEntry,
-    loopData
-  });
+// const app = express();
+const app = createExpressServer({
+  controllers: [SugarCollectorController, HomeController],
+  cors: true
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
